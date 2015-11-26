bb04ef0e79e72f67de27269ff7d97614dbb00808
Trenton Broughton <trenton@kindrid.com>
2015-11-26 01:02:30 -0500

    Complete rewrite to use canvas instead of DOM

--- static/main.js
+++ static/main.js
@@ -1,60 +1,104 @@
 var originX = 999;
 var originY = -1075;
+var posX = 0;
+var posY = 0;
+var tileSize = 513;
+var drawn = {};
+var ctx;
+var moveSpeed = 20;
 
 $(function() {
-    $(window).bind('mousewheel DOMMouseScroll', function(event){
-        var zoom = $("#container").css('zoom');
-        if (event.originalEvent.wheelDelta > 0 || event.originalEvent.detail < 0) {
-            zoom -= 1;
-            if(zoom <= 2) zoom = 2;
-            $("#container").css('zoom', zoom);
-        } else {
-            zoom += 1;
-            if(zoom >= 15) zoom = 15;
-            $("#container").css('zoom', zoom);
+    var canvas = document.getElementById("container")
+    ctx = canvas.getContext("2d")
+    $(document).keydown(function(e) {
+        // From http://stackoverflow.com/a/6011119/999844
+        switch(e.which) {
+            case 37: // left
+            posX -= moveSpeed;
+            break;
+
+            case 38: // up
+            posY -= moveSpeed;
+            break;
+
+            case 39: // right
+            posX -= moveSpeed;
+            break;
+
+            case 40: // down
+            posY += moveSpeed;
+            break;
+
+            default: return;
         }
+        getViewable();
+        e.preventDefault();
+    });
+    $(window).bind('mousewheel DOMMouseScroll', function(event){
+        if (event.originalEvent.wheelDelta > 0 || event.originalEvent.detail < 0) {
+            tileSize -= 5;
+            if(tileSize <= 100) tileSize = 100;
+        } else {
+            tileSize += 5;
+            if(tileSize >= 513) tileSize = 513;
+        }
+        event.preventDefault();
+        getViewable();
     });
     $(window).resize(function() {
         $("#canvas").height($(window).height());
-        getCanvas();
+        $("#container").height($(window).height());
+        $("#container").width($(window).width());
+        var canvas = document.getElementById("container");
+        canvas.width = $(window).width();
+        canvas.height = $(window).height();
+        getViewable();
     }).trigger('resize');
-    $('#container').draggable({
-        stop: function() {
-            getCanvas()
-        },
-        drag: function(e, ui) {
-            var zoom = $("#container").css('zoom');
-            var factor = (1 / zoom) -1;
-
-             ui.position.top += Math.round((ui.position.top - ui.originalPosition.top) * factor);
-             ui.position.left += Math.round((ui.position.left- ui.originalPosition.left) * factor);
-        }
-    });
+    $("#container").on('mousedown', function(e) {
+        $(this).addClass('dragging');
+        $(this).data('startX', e.offsetX).data('startY', e.offsetY);
+    }).on('mouseup', function() {
+        $(this).removeClass('dragging');
+    })
+    $(document).on('mousemove', '.dragging', function(e) {
+        var nX = e.offsetX - $(this).data('startX');
+        var nY = e.offsetY - $(this).data('startY');
+        posX -= nX;
+        posY -= nY;
+        $(this).data('startX', e.offsetX);
+        $(this).data('startY', e.offsetY);
+        getViewable();
+    })
 })
 
-function getCanvas() {
-    var x = -1 * Math.round($("#container").offset().left / 50);
-    var y = -1 * Math.round($("#container").offset().top / 50);
-    var w = Math.round($("#canvas").width() / 50);
-    var h = Math.round($("#canvas").height() / 50);
-    region(x, y, w, h);
+function drawTile(tileX, tileY) {
+    var x = (tileX - originX) * tileSize - posX;
+    var y = (tileY - originY) * tileSize - posY;
+    var src = "/1608/" + tileX + ":" + tileY + "+s.png";
+    if(drawn[src]) {
+        ctx.drawImage(drawn[src], x, y, tileSize, tileSize);
+        return;
+    }
+
+    var img = new Image();
+    img.onload = function() {
+        drawn[src] = img;
+        ctx.drawImage(img, x, y, tileSize, tileSize);
+    }
+    img.src = src;
 }
 
-function region(x, y, w, h) {
-    for(var wi = x; wi < x + w; wi++) {
-        for(var hi = y; hi < y + h; hi++) {
-            put(wi, hi);
+function getViewable() {
+    var w = $('#container').width();
+    var h = $('#container').height();
+
+    var startTileX = Math.floor(posX / tileSize) + originX;
+    var startTileY = Math.floor(posY / tileSize) + originY;
+    var endTileX = Math.ceil((posX + w) / tileSize) + originX;
+    var endTileY = Math.ceil((posY + h) / tileSize) + originY;
+    for(var x = startTileX; x < endTileX; x++) {
+        for(var y = startTileY; y < endTileY; y++) {
+            drawTile(x, y);
         }
     }
-}
-
-function put(x, y) {
-    if($('#' + x + '-' + y).length) return;
-    var cx = x * 50;
-    var cy = y * 50;
-    var px = x + originX;
-    var py = y + originY;
-    var src = "/1608/" + px + ":" + py + "+s.png";
-    var img = $("<img />").attr("src", src).css("left", cx).css("top", cy).attr('id', x + '-' + y).data('x', x).data('y', y);
-    $("#container").append(img);
 }
