3f54a8bc8c843f150e0f68f36931627c5733f0de
Trenton Broughton <trenton@kindrid.com>
2015-11-30 22:44:43 -0500

    Oops, fixed right arrow glitch

--- static/main.js
+++ static/main.js
@@ -22,7 +22,7 @@ $(function() {
             break;
 
             case 39: // right
-            posX -= moveSpeed;
+            posX += moveSpeed;
             break;
 
             case 40: // down
